94c9ad43ae234e49d3e6c957912d959e652b6cc3
Evan Hastings <evanrh@disroot.org>
2020-10-15 01:10:20 -0400

    Added full table for filling in values to update profile

--- includes/profile_update.inc.php
+++ includes/profile_update.inc.php
@@ -4,6 +4,12 @@
 
     if(isset($_POST['profile-update-submit'])) {
         // Get all filled in fields of the form
+        $email = $_POST['email'];
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $age = $_POST['age'];
+        $phone = $_POST['phone'];
+        $address = $_POST['address'];
     }
     else {
         header("Location: " . $parent);
--- profile_update.php
+++ profile_update.php
@@ -1,29 +1,42 @@
 <?php
-    session_start();
+    require_once "header.php";
+
     if(!isset($_SESSION['username'])) {
         header("Location: index.php");
         exit();
     }
 
-    require_once "header.php";
 ?>
     <h2>Update Profile Information</h2>
-    <p class="alert alert-info">Please check off all info you wish to update!</p>
     <form style="width:300px;" action="includes/profile_update.inc.php" method="POST">
         <div class="form-row">
-            <div class="form-group col-md-1">
-                <input type="checkbox" name="email-check" class="form-control-sm">
-            </div>
             <div class="form-group col-md-6">
                 <input type="email" name="email" class="form-control" placeholder="E-mail">
             </div>
         </div>
         <div class="form-row">
-            <div class="form-group col-md-1">
-                <input type="checkbox" name="email-check" class="form-control-sm">
-            </div>
             <div class="form-group col-md-6">
-                <input type="email" name="email" class="form-control" placeholder="E-mail">
+                <input type="text" name="fname" class="form-control" placeholder="First Name">
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <input type="text" name="lname" class="form-control" placeholder="Last Name">
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <input type="text" name="age" class="form-control" placeholder="Age">
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <input type="tel" name="phone" class="form-control" placeholder="Phone Number">
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <input type="text" name="address" class="form-control" placeholder="Address">
             </div>
         </div>
         <button type="submit" class="btn btn-primary" name="profile-update-submit" id="profile-update-submit">Submit</button>
